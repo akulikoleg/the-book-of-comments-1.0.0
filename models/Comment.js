aaad8b4f7952517dc9fa58c522b1a5f577046566
Oleg Aculov <oaculov@gmail.com>
2024-07-30 00:05:22 -0400

    server runned succes

--- models/Comment.js
+++ models/Comment.js
@@ -1,3 +1,4 @@
+/*
 const mongoose = require('mongoose');
 const {model, Schema} = require('mongoose');
 mongoose.Schema.Types.String.set('trim', true);
@@ -17,12 +18,39 @@ const commentSchema = new Schema({
         min: 0,
         max: 5,
     },
-    title: {
-        type: String,
-        trim: true,
-    },
+    title: String,
     description: String
 
-})
+});
 
-module.exports = model('Comment', commentSchema )
+module.exports = model('Comment', commentSchema );
+*/
+
+const { model, Schema} = require('mongoose')
+const mongoose = require("mongoose");
+mongoose.Schema.Types.String.set('trim', true);
+
+const commentSchema =
+
+  new Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+
+   createdAt: String,
+
+    rating: Number,
+
+    title: String,
+
+    description: String,
+
+  });
+
+module.exports = model('Comment', commentSchema)
+
+
+
