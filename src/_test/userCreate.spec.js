75454a619ae319cbb411043ac0b9ae36c0e1c9cf
Oleg Aculov <oaculov@gmail.com>
2024-08-05 23:18:25 -0400

    test dir created

---
+++ src/_test/userCreate.spec.js
@@ -0,0 +1,33 @@
+const request = require("supertest");
+const { expect } = require("chai");
+
+describe("User Create", () => {
+    describe("User Create - POSITIVE tests", () => {
+        it("user create all fields", () => {
+
+        })
+        it("user create all fields", () => {
+
+        })
+        it("user create all fields", () => {
+
+        })
+
+
+
+    })
+
+    describe("User Create - NEGATIVE tests", () => {
+        it("user create all fields", () => {
+
+        })
+        it("user create all fields", () => {
+
+        })
+        it("user create all fields", () => {
+
+        })
+
+    })
+
+})
