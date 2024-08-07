a678a96781fce1fb54a6def20b55263ee815c747
Oleg Aculov <oaculov@gmail.com>
2024-08-06 23:19:07 -0400

    userCreate.spec.js

---
+++ src/_test/user/queries.js
@@ -0,0 +1,10 @@
+const userCreateQ = `mutation UserCreate($userInput: UserFields) {
+     userCreate(userInput: $userInput) {
+     firstName
+     lastName
+      _id
+      }
+      }`
+
+
+module.exports = { userCreateQ }
